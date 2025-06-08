a498579b58b5aabf81275dc900363092b83ab201
Borja Castellano <borjacastellano1@gmail.com>
2025-06-08 22:48:58 +0200

    Image loading system updated. No more plugin conflicts

--- src/main/java/org/lebastudios/theroundtable/pluginaccounting/PluginAccounting.java
+++ src/main/java/org/lebastudios/theroundtable/pluginaccounting/PluginAccounting.java
@@ -25,7 +25,7 @@ public class PluginAccounting implements IPlugin
     {
         LabeledIconButton button = new LabeledIconButton(
                 "Accounting",
-                "accounting.png",
+                "accounting:accounting.png",
                 _ ->
                 {
                     MainStageController.getInstance().setCentralNode(new MainPaneController());
